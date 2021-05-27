4daf679db692c0ee4a6d2f79332a9c81a7e1ecbb
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-05-27 18:25:33 +0300

    Add files via upload

---
+++ ConsoleAppArraySınıfMetodlar/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleAppArraySınıfMetodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayiDizisi = { 23,12,4,86,72,3,11,17 };
+            Console.WriteLine("Sırasız Dizi");
+            foreach (var item in sayiDizisi) 
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("******************");
+            Console.WriteLine("Sıralı Dizi");
+            Array.Sort(sayiDizisi);
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("******************");
+
+            //Clear 
+            Console.WriteLine("****** Array Clear********");
+            Array.Clear(sayiDizisi, 2, 2); // 2. elemandan basla 2 eleman temizle == değerini sıfır yapıyor 
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************************");
+
+            //Reverse
+            Console.WriteLine("****** Reverse********");
+            Array.Reverse(sayiDizisi);  // diziyi tersine çeviriyor bastaki eleman sondaki oluyor 
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************************");
+
+            //IndexOf() 
+            Console.WriteLine("****** IndexOf()********");
+            int index=Array.IndexOf(sayiDizisi,23);  // virgülden sonraki elemanın kacıncı indexte olduğunu söylüyor 
+
+            Console.WriteLine(index);
+            Console.WriteLine("************************");
+
+            //Resize
+
+            Console.WriteLine("****** Resize********");
+            Array.Resize <int>(ref sayiDizisi, 9);  // Dizi 8 elemanlıydı 9 yaptık
+            sayiDizisi[8] = 99;
+            foreach (var item in sayiDizisi)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("************************");
+
+
+
+        }
+
+    }
+}
