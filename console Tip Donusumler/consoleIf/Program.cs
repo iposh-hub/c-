5eb8a8dadf6b4b641cc6e4403b762bb9dba861ba
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-05-27 13:14:52 +0300

    Add files via upload

---
+++ console Tip Donusumler/consoleIf/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleAppIfStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }
+            else if (time <= 18)
+            {
+                Console.WriteLine("İyi Günler");
+            }
+
+            else if (time > 18 && time<22 )
+            {
+                Console.WriteLine("İyi Aksamlar");
+            }
+
+            else 
+            {
+                Console.WriteLine("İyi Geceler");
+
+            }
+            string sonuc = time <= 18 ? "İyi Günler" : "İyi Aksamlar";
+            sonuc = hour >= 6 && hour < 11 ? "Günaydın" : hour <= 18 ? "İyi Günler" ;
+
+
+            Console.WriteLine(sonuc);
+            
+
+        }
+    }
+}
