ede7adb6592a6941e48b746391ff238b40b27114
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-05-28 17:15:47 +0300

    Add files via upload

---
+++ ConsoleAppEnum/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ConsoleAppEnum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int temp = 22;
+            if (temp <= (int)(HavaDurumu.Normal))
+            {
+
+                Console.WriteLine("Disari çikmak için biraz daha bekleyelim");
+            }
+
+            else if (temp > (int)HavaDurumu.Sicak) 
+            {
+                Console.WriteLine("Disari çikmak için çok sicak bir gün");
+
+            }
+
+            else if (temp>= (int)HavaDurumu.Normal && temp < (int)HavaDurumu.Sicak)
+            {
+                Console.WriteLine("Hadi disari cikalim!");
+
+            }
+
+        }
+    }
+
+    enum Gunler 
+    {
+    
+        Pazartesi=1,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma=23,
+        Cumartesi,
+        Pazar,
+    
+    }
+
+    enum HavaDurumu
+    {
+        Soguk=5,
+        Normal=20,
+        Sicak= 25,
+        CokSicak=30,
+    }
+}
