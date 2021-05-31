cdc6deb2dfc434880c2558bb78efea90a0d03f73
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-05-31 14:33:35 +0300

    Add files via upload

---
+++ ConsoleAppDictionary/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //system.collection.generic
+            Dictionary<int, string> users = new Dictionary<int, string>();
+            users.Add(1, "Ayse Yılmaz");
+            users.Add(2, "Ahmet Kaya");
+            users.Add(3, "Yıldırım Kaymaz");
+            users.Add(4, "Ozcan Cosar");
+            //Dizinin elemanlarına erişim 
+            Console.WriteLine("***Elemanlara Erişim****");
+            Console.WriteLine(users[3]); // indexini yazıyorsun
+            foreach (var item in users) 
+            {
+
+                Console.WriteLine(item);
+            }
+
+            //Count;
+
+            Console.WriteLine("***Count****");
+
+            Console.WriteLine(users.Count);
+            //Remove 
+            Console.WriteLine("*****Remove****");
+            users.Remove(2);
+            foreach (var item in users)
+            {
+
+                Console.WriteLine(item);
+            }
+
+            //Keys
+            Console.WriteLine("****Keys***");
+            foreach (var item in users.Keys)
+            {
+
+                Console.WriteLine(item);
+            }
+
+            //Values
+            Console.WriteLine("****Values***");
+
+            foreach (var item in users.Values)
+            {
+
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
