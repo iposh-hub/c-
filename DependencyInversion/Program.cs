283d16d6aa424aa541cf0ceb283f32acca0539cb
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-06-06 16:39:07 +0300

    Add files via upload

---
+++ DependencyInversion/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace DependencyInversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greetings greetings = new Greetings(new GreetingsInSpanish());
+            greetings.PrintGreetings();
+        }
+    }
+
+    
+    public class Greetings 
+    {
+        private IGreeting greetPerson;
+        public Greetings (IGreeting greeting) 
+        {
+            this.greetPerson = greeting;
+        }
+
+        public void PrintGreetings() 
+        {
+            greetPerson.Greet();
+        
+        }
+    
+    
+    }
+    
+    
+    
+   
+    
+    public interface IGreeting 
+    {
+        void Greet();
+    
+    }
+
+    public class GreetingsInEnglish : IGreeting
+    {
+        public void Greet()
+        {
+            Console.WriteLine( "Hello, How's your day beenn?");
+        }
+    }
+
+    public class GreetingsInItalian : IGreeting
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Ciao, Come è stata la tua giornata? ");
+        }
+    }
+
+    public class GreetingsInSpanish : IGreeting
+    {
+        public void Greet()
+        {
+            Console.WriteLine("Hola, Coma ha sido tu dia?");
+        }
+    }
+}
