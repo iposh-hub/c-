17eddb8bcfe51d8005021ad9658da3c73395dca4
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-06-06 15:26:39 +0300

    Add files via upload

---
+++ GreetingProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GreetingProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Greetings greetings = new Greetings();
+            greetings.Greet();
+        }
+
+
+        public class Greetings
+        {
+            
+            public void Greet() 
+            {
+                EnglishGreeting englishGreeting = new EnglishGreeting();
+                englishGreeting.GreetingsInEnglish();
+
+            }  
+
+        }
+
+        public class EnglishGreeting 
+        {
+            public void GreetingsInEnglish()
+            {
+                Console.WriteLine("Hello, How's your day been?");
+
+            }
+
+        }
+    }
+}
