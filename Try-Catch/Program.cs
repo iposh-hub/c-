4026d8d360987b0989cea0552f399f98e062f81d
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-06-11 19:06:13 +0300

    Add files via upload

---
+++ Try-Catch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.Write("a: ");
+                int a = int.Parse(Console.ReadLine());
+
+                Console.Write("b: ");
+                int b = int.Parse(Console.ReadLine());
+
+                int c = a + b;
+
+                Console.WriteLine(c);
+
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine("Bir Hata Oluştu: " + ex.Message);
+            }
+
+            finally 
+            {
+                Console.WriteLine("İşlem tamamlandı.");
+            }
+        }
+    }
+}
